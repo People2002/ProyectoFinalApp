b7db2396d809a9934913f403761b1cec6fcf6627
People2002 <a.zelaya.p.j@gmail.com>
2022-06-06 01:03:53 -0500

    VentanaAlmacen

---
+++ app/src/main/java/com/andy/proyectofinalapp/Almacen_1.java
@@ -0,0 +1,14 @@
+package com.andy.proyectofinalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Almacen_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_almacen1);
+    }
+}
