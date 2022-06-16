c93245b073a379a6f4dcd73d192c4fdc9e3b4f6b
People2002 <a.zelaya.p.j@gmail.com>
2022-06-16 11:41:34 -0500

    Jueves16

--- app/src/main/java/com/andy/proyectofinalapp/Login_Trabajador_Opciones.java
+++ app/src/main/java/com/andy/proyectofinalapp/Login_Trabajador_Opciones.java
@@ -2,13 +2,34 @@ package com.andy.proyectofinalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_Trabajador_Opciones extends AppCompatActivity {
+
+    Button btnAlmac;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_trabajador_opciones);
+        asignarReferencias();
+    }
+
+    private void asignarReferencias() {
+        btnAlmac = findViewById(R.id.btnAlmacenTrabajadorOpcion);
+
+        btnAlmac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_Trabajador_Opciones.this, Almacen_1.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/andy/proyectofinalapp/Solicitud_Trabajador_1.java
@@ -0,0 +1,14 @@
+package com.andy.proyectofinalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Solicitud_Trabajador_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitud_trabajador1);
+    }
+}
