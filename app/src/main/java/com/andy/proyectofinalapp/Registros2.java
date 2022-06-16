914c35efe1bd35056722b4e455a7c88008cb2c89
Nigma7 <zamoraramosadrian@gmail.com>
2022-06-16 11:57:49 -0500

    Jueves2

---
+++ app/src/main/java/com/andy/proyectofinalapp/Registros2.java
@@ -0,0 +1,14 @@
+package com.andy.proyectofinalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registros2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registros2);
+    }
+}
---
+++ app/src/main/java/com/andy/proyectofinalapp/eliminar4.java
@@ -0,0 +1,14 @@
+package com.andy.proyectofinalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class eliminar4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar4);
+    }
+}
