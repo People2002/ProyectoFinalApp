708c2232389877cc1b0a325a432908dd4ec56473
TwiLight24 <se_edu@hotmail.com>
2022-07-02 18:23:11 -0500

    Avance BD 02/07/2022 version 6

--- app/src/main/java/com/andy/proyectofinalapp/Notificaciones1.java
+++ app/src/main/java/com/andy/proyectofinalapp/Notificaciones1.java
@@ -1,14 +1,40 @@
 package com.andy.proyectofinalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.andy.proyectofinalapp.entidad.Registro;
+import com.andy.proyectofinalapp.modelo.DAORegistro;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Notificaciones1 extends AppCompatActivity {
+    RecyclerView rvProdcutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificaciones1);
+        asignarReferencias();
+        mostrarProductos();
+    }
+
+    private void mostrarProductos() {
+        List<Registro> listaregistros = new ArrayList<>();
+        DAORegistro daoRegistro = new DAORegistro(this);
+        AdaptadorPersonalizadoRegistros adaptador;
+
+        daoRegistro.abriBD();
+        listaregistros = daoRegistro.cargarRegistros();
+        adaptador = new AdaptadorPersonalizadoRegistros(Notificaciones1.this, listaregistros);
+        rvProdcutos.setAdapter(adaptador);
+        rvProdcutos.setLayoutManager(new LinearLayoutManager(Notificaciones1.this));
+    }
+    private void asignarReferencias(){
+        rvProdcutos = findViewById(R.id.rv_productos_regis2);
     }
 }
